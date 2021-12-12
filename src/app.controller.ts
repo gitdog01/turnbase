5850b1b8fd008a84c3f3ec1f29b87a02c3a61a25
mononomo <hostdog01@naver.com>
2021-12-12 21:43:53 +0900

    feat(git) : env 수정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+
+// router
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('help/:id')
+  getHello(@Req() request: Request): string {
     return this.appService.getHello();
   }
 }
