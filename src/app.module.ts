5864f6e5319713ac9d0eb20a13019c7501760b9b
mononomo <hostdog01@naver.com>
2021-12-13 23:43:18 +0900

    feat(middleWare) : logger 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,24 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/game-project', {
       useNewUrlParser: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
+{
+}
